d2ff0b074ad665356abf7f0b44771ead495e685e
Roshni Sahoo <roshnis@twix.csail.mit.edu>
2020-02-04 23:30:12 -0500

    added updated train and experiments script

--- train.py
+++ train.py
@@ -25,8 +25,7 @@ ranking_output_dir = "./outputs/ranking_outputs"
 ranked_indices_output_dir = "./top_ten_augs"
 aggregated_outputs_dir = "./outputs/aggregated_outputs/"
 
-tta_functions = tta.base.Compose([ tta.transforms.FiveCrops(224, 224), tta.transforms.HorizontalFlip(), 
-                                  tta.transforms.ColorJitter(), tta.transforms.Rot([2,3])])
+tta_functions = tta.base.Compose([tta.transforms.Rot([(i-0.5, i+0.5) for i in range(-15, 16)])])
 
 # Set up directories
 if not os.path.exists(train_output_dir):
@@ -45,20 +44,21 @@ model = get_pretrained_model(model_name)
 tta_model = tta.ClassificationTTAWrapperOutput(model, tta_functions, ret_all=True)
 tta_model.to('cuda:0')
 
-aug_order = ['five_crop', 'hflip', 'colorjitter', 'rotation']
+aug_order = ['rotation']
 aug_list = write_aug_list(tta_model.transforms.aug_transform_parameters,aug_order)
 np.save('./aug_list', aug_list)
 np.save('./aug_order', aug_order)
 print("[X] Model loaded!")
 
 # Generate validation outputs
-# TODO: Move imnet dataloader to be within function for model_name; cleaner train.py file,  
+# TODO: Move imnet dataloader to be within function for model_name; cleaner train.py file, 
 output_file = val_output_dir + "/" + model_name + ".h5"
 if not check_if_finished(output_file): 
     dataloader = get_imnet_dataloader(val_dir, batch_size=2) 
     write_augmentation_outputs(tta_model, dataloader, output_file)
 print("[X] Val outputs written!")
 
+"""
 # Generate training outputs
 output_file = train_output_dir + "/" + model_name + ".h5"
 if not check_if_finished(output_file): 
@@ -67,14 +67,14 @@ if not check_if_finished(output_file):
     write_augmentation_outputs(tta_model, dataloader, output_file)
 print("[X] Train outputs written!")
 
-aug_names = ['combo']
-rank_names = ['OMP', 'LR', 'APAC']
+aug_names = ['rotation']
+rank_names = ['LR']
 for rank_name in rank_names:
     for aug_name in aug_names:
         print("RANK ALG: ", rank_name, "\tAUG: ", aug_name)
         if not os.path.exists(ranking_output_dir + "/" + model_name + "/"  + rank_name+ '/'+ aug_name + ".h5"):
             write_ranking_outputs(model_name, aug_name, rank_name)
-        if not os.path.exists('./agg_models/ranking/' + aug_name + '/' + rank_name+ '/' + model_name + '/9.pth'):
+        if not os.path.exists('./agg_models/ranking/' + aug_name + '/' + rank_name+ '/' + model_name + '.pth'):
             train_ranked_lrs(model_name, aug_name, rank_name)
         print("[X] Ranking outputs written!")
         print("[X] Ranked logistic regressions trained!")
@@ -88,3 +88,4 @@ print("[X] Aggregated outputs written + evaluated!")
 
 evaluate_threshold(model_name)
 print("[X] Thresholding evaluated!")
+"""
